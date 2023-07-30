553c892c81136d0d4dc844fff5107259ade23b60
sreelekshmi1 <sreelekshmip808@gmail.com>
2023-07-30 14:49:37 -0400

    user registration

---
+++ mainapp/forms.py
@@ -0,0 +1,16 @@
+
+# myapp/forms.py
+from django import forms
+from .models import User,Login,City
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label='Select City', to_field_name='name')
+        fields = ['firstname', 'lastname', 'streetaddress', 'city', 'province', 'zipcode', 'email', 'mobile']
+        
+
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+# myapp/views.py
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.db import connection
+from .models import Login
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            Login.objects.create(User_Id=user, username=username, password=password)
+            return redirect('login')  # Replace 'login' with your login URL name
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration.html', {'form': form})
+  
+def login(request):
+    #if request.method=='POST':
+       #code to be updated
+    return render(request, 'login.html')
