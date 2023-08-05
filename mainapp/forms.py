e24a2427ae2a43e945c95cac7b67a87aa4485864
sreelekshmi1 <sreelekshmip808@gmail.com>
2023-08-05 11:47:41 -0400

    log file creation and login

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,7 +1,7 @@
 
 # myapp/forms.py
 from django import forms
-from .models import User,Login,City
+from .models import User,City
 
 class UserRegistrationForm(forms.ModelForm):
     username = forms.CharField(max_length=20)
@@ -11,7 +11,15 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         city = forms.ModelChoiceField(queryset=City.objects.all(), empty_label='Select City', to_field_name='name')
-        fields = ['firstname', 'lastname', 'streetaddress', 'city', 'province', 'zipcode', 'email', 'mobile']
+        fields = ['firstname', 'lastname', 'streetaddress', 'city', 'province', 'zipcode', 'email', 'mobile','username','password']
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data['password']
+        user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
         
 
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,25 +1,38 @@
 
+import logging
 
 # myapp/views.py
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.db import connection
-from .models import Login
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            Login.objects.create(User_Id=user, username=username, password=password,status=0)
             return redirect('login')  # Replace 'login' with your login URL name
     else:
         form = UserRegistrationForm()
     return render(request, 'registration.html', {'form': form})
   
+# views.py
+
+
 def login(request):
-    #if request.method=='POST':
-       #code to be updated
+    if request.method == 'POST':
+        logger = logging.getLogger('mainapp')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        logger.debug(f'Attempting login for user: {username}')
+        if user is not None:
+            logger.info(f'User {username} successfully authenticated.')
+            return redirect('Home.html')  # Redirect to the homepage after successful login
+        else:
+            logger.warning(f'Failed login attempt for user: {username}')
+            return render(request, 'login.html', {'error_message': 'Invalid credentials'})
     return render(request, 'login.html')
+
